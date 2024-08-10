147d28e92db2b7d2ba95055a2e968645f16668b9
rithen <rithen1971@gmail.com>
2024-08-10 12:35:00 +0600

    passwordsalting

---
+++ passwordsalting.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+void salt(char pass[]);
+
+int main(){
+
+char pass[100];
+
+printf("Enter your password\n-");
+scanf("%s",pass);
+
+salt(pass);
+
+
+
+    return 0;
+}
+
+void salt(char pass []) {
+
+char newpass[200];
+char salt[100] = {"16ÖJ¤#"};
+
+strcpy(newpass , pass);
+
+strcat(newpass , salt);
+
+printf("User pass:");
+puts(newpass);
+
+}
